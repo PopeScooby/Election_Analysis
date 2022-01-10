440a9d85974ecb24847cd1a8b3cab949c67a4dd4
Pope Scooby <PopeScooby@gmail.com>
2022-01-09 19:08:08 -0600

    module started

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complte list of candidates who received votes
+# 3. The percentage of votes each cadidate won
+# 4. The total number of votes each candidate won
+# 5. The Winner of the election based on popular vote
